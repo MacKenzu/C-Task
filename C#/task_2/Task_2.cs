c43342bed1eb345dadfd077ac2c3832b9299dc15
Hbook <pa4itsky.jaroslav@gmail.com>
2024-01-26 15:00:24 +0300

    Task_2 complit

--- C#/task_2/Task_2.cs
+++ C#/task_2/Task_2.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Дублирование нулей в масииве*/
+
+class Task_2
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[] { 1, 0, 2, 3, 0, 5, 0 };
+        printArray(array);
+        System.Console.WriteLine();
+        dublerZero(array);
+        printArray(array);
+
+    }
+    public static void printArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            System.Console.Write(num + " ");
+        }
+    }
+    public static void dublerZero(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == 0)
+            {
+                for (int j = arr.Length - 2; j > i; j--)
+                {
+                    arr[j + 1] = arr[j];
+
+                }
+                arr[i + 1] = 0;
+                i += 2;
+            }
+        }
+    }
+}
