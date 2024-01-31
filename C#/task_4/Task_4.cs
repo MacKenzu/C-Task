6191f5fc644a720dcbe9a9fad7216c5d121464b8
Hbook <pa4itsky.jaroslav@gmail.com>
2024-01-31 15:44:11 +0300

    finished task_4

---
+++ C#/task_4/Task_4.cs
@@ -0,0 +1,35 @@
+﻿class Task_3
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[] { 1, 2, 2, 3, 4, 4, 3, 3, 2, 2 };
+        int num = 2;
+        printArray(array);
+        System.Console.WriteLine();
+        System.Console.WriteLine(RemoveElement(array, num));
+        printArray(array);
+    }
+    public static void printArray(int[] arr)
+    {
+        System.Console.Write("[");
+        foreach (int num in arr)
+        {
+            System.Console.Write(num + ", ");
+        }
+        System.Console.Write("]");
+    }
+    public static int RemoveElement(int[] nums, int val)
+    {
+        int count = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[count++] = nums[i];
+            }
+        }
+        return count;
+    }
+}
+
